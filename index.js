2623eec7c90b99fe087d52aae664c4c66660ec58
Iisakki Kulhua <iisakki@kulhua.net>
2021-01-03 22:37:29 +0200

    Osan 3 tehtävät 3.17 asti, henkilöiden määrä ei toimi

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 const Person = require('./models/person')
+const { response } = require('express')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
 
 morgan.token('person', (req, res) => JSON.stringify(req.body))
 
@@ -27,36 +28,43 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
+const getDocumentCount = () => {
+    
+    Person.countDocuments({}, (err, result) => {
+        console.log(result.toString())
+
+        return result
+    })
+}
+
 app.get('/info', (req, res) => {
 
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    res.send(`<p>Phonebook has info for ${getDocumentCount()} people</p>
               <p>${new Date().toString()}</p>`)
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    
-    Person.findById(req.params.id).then(person => {
-        
-        if (person) {
-            res.json(person)
-        } else {
-            res.status(404).end()
-        }
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+
+    Person.findById(req.params.id)
+        .then(person => {
+
+            if (person) {
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (req, res, next) => {
 
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-/* const generateId = () => {
-    const id = Math.floor(Math.random() * 1000000)
-
-    return id
-} */
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
@@ -67,12 +75,6 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    /* if (persons.some(person => person.name === body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    } */
-
     const person = new Person({
         name: body.name,
         number: body.number
@@ -82,6 +84,40 @@ app.post('/api/persons', (req, res) => {
         res.status(201).json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message)
+
+    if (err.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(err)
+}
+
+app.use(errorHandler)
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
