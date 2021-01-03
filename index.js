19946d194c4f130d85d31c848b79938ee01c48ce
Iisakki Kulhua <iisakki@kulhua.net>
2021-01-03 13:49:25 +0200

    Heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('person', (req, res) => JSON.stringify(req.body))
 
@@ -100,7 +102,7 @@ app.post('/api/persons', (req, res) => {
     res.status(201).json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
